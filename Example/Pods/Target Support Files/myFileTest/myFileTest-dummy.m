1bf4d6692bde348ccfa5664cdc8751fee8ddaead
renrenshipin <tangqinghuan@rrmj.tv>
2019-04-25 18:12:23 +0800

    testfile push

---
+++ Example/Pods/Target Support Files/myFileTest/myFileTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_myFileTest : NSObject
+@end
+@implementation PodsDummy_myFileTest
+@end
---
+++ myFileTest/Classes/TQHViewController.h
@@ -0,0 +1,13 @@
+//
+//  TQHViewController.h
+//  myFileTest
+//
+//  Created by tanquwell on 04/25/2019.
+//  Copyright (c) 2019 tanquwell. All rights reserved.
+//
+
+@import UIKit;
+
+@interface TQHViewController : UIViewController
+
+@end
